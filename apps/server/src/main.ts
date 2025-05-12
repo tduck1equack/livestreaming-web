1b21d776f3be1591d88d5a5d1881d9e16ffabb16
tduck1equack <golden4gnvx@gmail.com>
2025-05-13 00:01:26 +0700

    chore(cookie-parser): Enable cookie-parser middleware

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
 import { ConsoleLogger, Logger, ValidationPipe } from "@nestjs/common";
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -13,7 +14,10 @@ async function bootstrap() {
   });
   const logger = new Logger("Bootstrap");
 
-  // Use NestJS's built-in Pipes
+  // Enable CORS so the web can access the API
+  app.enableCors();
+  app.use(cookieParser());
+  // Use NestJS's built-in Pipes in place of Zod
   app.useGlobalPipes(
     new ValidationPipe({
       // Prevent unnecessary request params
@@ -23,15 +27,13 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const apiPrefix = configService.get<string>("API_ENDPOINT") || "api/v1";
+  // Fallback to default port 3000 if environment is not set
   const serverPort = configService.get<number>("SERVER_PORT") ?? 3000;
   app.setGlobalPrefix(apiPrefix);
 
   logger.log(
     `API up and running at port ${serverPort} and endpoint root ${apiPrefix}`,
   );
-
-  // Fallback to default port 3000 if environment is not set
-
   app.listen(serverPort);
 }
 bootstrap();
