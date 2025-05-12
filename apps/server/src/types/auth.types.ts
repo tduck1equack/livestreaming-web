117bae6ee5db41d8a60ca8eb41fd6e937e2130d2
tduck1equack <golden4gnvx@gmail.com>
2025-05-13 00:02:07 +0700

    fix: minor fixes for types related to auth

--- apps/server/src/types/auth.types.ts
+++ apps/server/src/types/auth.types.ts
@@ -1,17 +1,26 @@
-/**Represents a payload to be signed and assigned a JWT */
+/**
+ * @type Represents a payload to be signed and assigned a JWT
+ * and also intercepted by other middlewares
+ *
+ */
 export type JwtPayload = {
   sub: string;
   email: string;
   username: string;
+  roles: number[];
   iat: number;
 };
 /**Return type for refresh token strategy, with a refresh token attached */
-export type JwtPayloadWithRefreshToken = {
+export type JwtPayloadWithTokens = {
   payload: JwtPayload;
+  accessToken: string | undefined;
   refreshToken: string | undefined;
 };
 /**Return type for signToken*/
 export type TokenResponse = {
   accessToken: string;
   refreshToken: string;
+};
+export type PassportValidatedPayload = {
+  user: JwtPayload;
 };
